38137848f7655b1534df6729cf4e34a73f07991a
LuckySahudev <sluckysahu18@gmail.com>
2026-03-22 00:16:02 +0530

    add db port

--- property_api/config/db.js
+++ property_api/config/db.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
@@ -11,10 +11,9 @@ const db = mysql.createConnection({
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err);
-    return;
+  } else {
+    console.log("MySQL Connected");
   }
-
-  console.log("MySQL Connected ✅");
 });
 
 module.exports = db;
